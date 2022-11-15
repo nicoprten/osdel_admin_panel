7305d9f7d1264d77c76f831758d0b24924c393c2
nicoprten <nicoprtenjaca@gmail.com>
2022-11-15 14:20:10 -0300

    News, first menu ui

---
+++ src/components/Auths.js
@@ -0,0 +1,7 @@
+export default function Auths(){
+    return(
+        <div>
+            <h2>Autorizaciones</h2>
+        </div>
+    )
+}
---
+++ src/components/MedicalChart.js
@@ -0,0 +1,7 @@
+export default function MedicalChart(){
+    return(
+        <div>
+            <h2>Cartilla médica</h2>
+        </div>
+    )
+}
